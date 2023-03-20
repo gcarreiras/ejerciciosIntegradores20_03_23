3c3b1dda08626bc1e2a4186ea3e4386cd08c3994
gcarreiras <gcarreiras86@gmail.com>
2023-03-20 18:53:44 -0300

    ejercicio01

--- ejercicio01.py
+++ ejercicio01.py
@@ -1,1 +1,11 @@
 # Escribir una función que calcule el máximo común divisor entre dos números.
+
+
+def calcularmcd(x, y):
+    if y == 0:
+        return x
+    else:
+        return calcularmcd(y, x % y)
+    
+    
+print(calcularmcd(12,36))
